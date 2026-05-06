bb2325df7bc773d18d36536b4ff768e689ef5b5e
avaNeon <163699989+avaNeon@users.noreply.github.com>
2026-05-06 08:43:07 +0800

    feat(heartbeat): implement heartbeat consumer with Redis repository
    Consume heartbeat messages from RabbitMQ and persist the latest
    heartbeat timestamp for each user-video-session into a Redis ZSet.

--- nilo-mq-consumer/src/main/java/com/neon/nilomqconsumer/consumer/HeartbeatConsumer.java
+++ nilo-mq-consumer/src/main/java/com/neon/nilomqconsumer/consumer/HeartbeatConsumer.java
@@ -1,4 +1,35 @@
 package com.neon.nilomqconsumer.consumer;
 
-public class HeartbeatConsumer {
+import com.neon.nilocommon.entity.constants.RedisKey;
+import com.neon.nilomqconsumer.repository.redis.HeartbeatRedisRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import static com.neon.nilocommon.entity.constants.MqInfo.VIDEO_HEARTBEAT_QUEUE;
+
+@RequiredArgsConstructor
+@Component
+public class HeartbeatConsumer
+{
+    private final HeartbeatRedisRepository heartbeatRedisRepository;
+
+    @RabbitListener(queues = VIDEO_HEARTBEAT_QUEUE)
+    public void processHeartbeat(String message)
+    {
+        // 解析消息 "videoId:fileIndex:userId:timestamp"
+        String[] parts = message.split(":");
+        if (parts.length == 4)
+        {
+            String videoId = parts[0];
+            String fileIndex = parts[1];
+            String sessionId = parts[2];
+            long timestamp = Long.parseLong(parts[3]);
+            // key 包含了 videoId 和 fileIndex
+            // 记录的粒度到每个视频文件
+            String key = RedisKey.VIDEO_HEARTBEAT_PREFIX + videoId + ":" + fileIndex;
+            // 写入redis
+            heartbeatRedisRepository.saveHeartbeat(key, sessionId, timestamp);
+        }
+    }
 }
--- nilo-mq-consumer/src/main/java/com/neon/nilomqconsumer/repository/redis/HeartbeatRedisRepository.java
+++ nilo-mq-consumer/src/main/java/com/neon/nilomqconsumer/repository/redis/HeartbeatRedisRepository.java
@@ -1,4 +1,28 @@
 package com.neon.nilomqconsumer.repository.redis;
 
-public class HeartbeatRedisRepository {
+import com.neon.nilocommon.entity.constants.RedisKey;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+@RequiredArgsConstructor
+@Repository
+public class HeartbeatRedisRepository
+{
+    private final RedisTemplate <String, Object> redisTemplate;
+
+    /**
+     * 将心跳信息写入 Redis
+     *
+     * @param key       redis key
+     * @param sessionId 会话ID
+     * @param timestamp 创建时的时间戳
+     */
+    public void saveHeartbeat(String key, String sessionId, long timestamp)
+    {
+        // 写入 ZSet O(log(N))
+        redisTemplate.opsForZSet().add(key, sessionId, timestamp);
+        // 优化：维护一个 Set，包含所有有人在看的视频对应在 ZSET 的 key，方便后续定时清理任务，避免全量扫描 Redis Key
+        redisTemplate.opsForSet().add(RedisKey.VIDEO_ACTIVE_LIST, key);
+    }
 }

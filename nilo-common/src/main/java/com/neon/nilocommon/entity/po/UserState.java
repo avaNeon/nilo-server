1d591204a76948368cc951efac8d99a7c5041f1f
avaNeon <163699989+avaNeon@users.noreply.github.com>
2026-05-06 08:42:26 +0800

    feat(user-state): add UserState POJO for user statistics
    A POJO containing followerCount, followingCount, and currentCoin for
    caching frequently-changing user statistics in Redis.

--- nilo-common/src/main/java/com/neon/nilocommon/entity/po/UserState.java
+++ nilo-common/src/main/java/com/neon/nilocommon/entity/po/UserState.java
@@ -1,4 +1,22 @@
 package com.neon.nilocommon.entity.po;
 
-public record UserState() {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * 用户统计信息PO
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserState
+{
+    private Integer followerCount;
+
+    private Integer followingCount;
+
+    private Integer currentCoin;
 }

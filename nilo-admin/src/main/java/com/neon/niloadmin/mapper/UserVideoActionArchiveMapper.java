e02c82a959d2524a0ca0deaa442c24584bd8fa1f
avaNeon <163699989+avaNeon@users.noreply.github.com>
2026-05-14 10:08:19 +0800

    feat(admin-archive): add archive mappers for user actions
    Add admin archive mappers and XML mappings for archived comment and video user actions.
    
    Support full recovery of engagement data when restoring deleted videos.

---
+++ nilo-admin/src/main/java/com/neon/niloadmin/mapper/UserVideoActionArchiveMapper.java
@@ -0,0 +1,29 @@
+package com.neon.niloadmin.mapper;
+
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * 用户视频行为 点赞、收藏、投币 数据库操作接口
+ */
+public interface UserVideoActionArchiveMapper<T, P> extends BaseMapper <T, P>
+{
+
+    /**
+     * 根据ActionId更新
+     */
+    Integer updateByActionId(@Param("bean") T t, @Param("actionId") Long actionId);
+
+
+    /**
+     * 根据ActionId删除
+     */
+    Integer deleteByActionId(@Param("actionId") Long actionId);
+
+
+    /**
+     * 根据ActionId获取对象
+     */
+    T selectByActionId(@Param("actionId") Long actionId);
+
+
+}

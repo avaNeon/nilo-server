20612ee43a9e9ca2fa680c0dd1fa3ed69e6b4b87
avaNeon <163699989+avaNeon@users.noreply.github.com>
2026-07-03 08:27:30 +0800

    feat(storage) add new storage microservice

---
+++ nilo-storage/src/main/java/com/neon/nilostorage/NiloStorageApplication.java
@@ -0,0 +1,15 @@
+package com.neon.nilostorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiloStorageApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(NiloStorageApplication.class, args);
+    }
+
+}
---
+++ nilo-storage/src/test/java/com/neon/nilostorage/NiloStorageApplicationTests.java
@@ -0,0 +1,15 @@
+package com.neon.nilostorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiloStorageApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
